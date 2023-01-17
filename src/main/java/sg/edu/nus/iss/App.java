4f6f0269588eb2ef822b2816bb6bc459e474cb05
Charleswcl <wang90cl@hotmail.com>
2023-01-17 15:22:28 +0800

    client01

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,14 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Hello world!
  */
@@ -9,9 +18,29 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
+     * @throws IOException
+     * @throws UnknownHostException
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+
+        Socket socket = new Socket("localhost", 7000);
+
+        try (OutputStream os = socket.getOutputStream()) {
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
+
+            Console con = System.console();
+            String readInput = "";
+
+            while (!readInput.equalsIgnoreCase("close")) {
+                readInput = con.readLine("Enter a command: ");
+                dos.writeUTF(readInput);
+                dos.flush();
+            }
+        } catch (EOFException ex) {
+            socket.close();
+        }
     }
 }
